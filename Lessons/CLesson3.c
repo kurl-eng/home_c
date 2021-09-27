b7584247596dc24ba35fd6bd278000c0dbceecb1
Kurl <kksbnn4@gmail.com>
2021-09-27 18:42:08 +0300

    Lesson 3
    practice

---
+++ Lessons/CLesson3.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+
+int main(void)
+{   
+    
+
+    /*
+    int seconds = 12334;
+    int hours = seconds / 60 / 60;
+    int minutes = seconds % 3600 / 60;
+
+    printf("Hours : %d Minutes %d\n", hours, minutes);
+    */
+
+    /*
+    float f1 = 123.64;
+    int k, fint;
+
+    fint = f1;
+    fint *= 10;
+    f1 *= 10;
+    k = f1 - fint;
+
+    printf("result %d\n", k);
+    */
+
+    /*
+    int x = 123;
+    int k = 0;
+
+    k += x % 10;
+    x /= 10;
+    k += x % 10;
+    x /= 10;
+    k+= 1;
+
+    printf("result %d\n", k);
+    */
+
+    /*
+    int k;
+    int x = 123456;
+
+    k = x / 100;
+
+    k = k % 10;
+
+    printf("2nd range number is %d\n", k);
+    */
+
+    return 0;
+}
