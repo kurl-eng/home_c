e4206631bf0b3cc7ee1cc307ab3461b88cdc4e0e
Kurl <kksbnn4@gmail.com>
2021-09-30 05:00:44 +0300

    Fixing 2ng homework
    and starting 4th HW

--- home4/main.c
+++ home4/main.c
@@ -3,18 +3,18 @@
 
 int main()
 {
-    uint16_t num, val;
-    int shift;
+    unsigned int num = 0, val = 0;
+    int shift = 0;
 
     printf("Enter number and shift value\n");
 
     scanf("%x%d", &num, &shift);
 
     if (shift > 0) {
-        val = num >> shift;
+        val = num << shift;
         printf("Input: %x shift right: %d\n Result: %x\n", num, shift, val);
     } else if (shift < 0) { 
-        val = num << shift;
+        val = num >> shift;
         printf("Input: %x shift left: %d\n Result: %x\n", num, shift, val);
     } else {
         printf("Input: %x shift: %d\n", num, shift);
