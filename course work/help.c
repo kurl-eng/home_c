569b9690417954ff7104bed70dc470768476525d
Kurl <kksbnn4@gmail.com>
2021-11-03 18:23:21 +0300

    f

--- course work/help.c
+++ course work/help.c
@@ -6,6 +6,7 @@ void help_com(const char *usage)
 {
     printf("HELP MENU:\n");
     printf("To print file data, type: -f <filename.csv>\n");
-    printf("WARNING the key -f is required\n");
+    printf("WARNING! The key -f is required\n");
     printf("To print month statistic of file, type: -m [month]\n");
+    printf("Program emulate current year only with name <year.csv>\n");
 }
